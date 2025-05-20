0484c7ad7fd308165ada59076b666327b685154d
CARLOS_LAP\cone_ <carloscastillodeveloper@gmail.com>
2025-05-20 13:07:39 -0600

    debug del token rest client

--- src/main/java/org/mch/rest/client/JwtPropagator.java
+++ src/main/java/org/mch/rest/client/JwtPropagator.java
@@ -3,10 +3,12 @@ package org.mch.rest.client;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MultivaluedMap;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.rest.client.ext.ClientHeadersFactory;
 import org.mch.rest.security.TokenHolder;
 
 @ApplicationScoped
+@Slf4j
 public class JwtPropagator implements ClientHeadersFactory {
 
     @Inject
@@ -16,6 +18,8 @@ public class JwtPropagator implements ClientHeadersFactory {
     public MultivaluedMap<String, String> update(MultivaluedMap<String, String> incomingHeaders,
                                                  MultivaluedMap<String, String> clientOutgoingHeaders) {
         String token = tokenHolder.getToken();
+
+        log.info("EL TOKEN: {}", token);
 
         if (token != null && !token.isBlank()) {
             clientOutgoingHeaders.putSingle("Authorization", token);
